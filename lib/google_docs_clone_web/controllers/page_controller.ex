6b3d38ab10ce0926f51b4ce81cfd5082463cf39d
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-12-01 19:10:36 +0530

    add home page

--- lib/google_docs_clone_web/controllers/page_controller.ex
+++ lib/google_docs_clone_web/controllers/page_controller.ex
@@ -2,7 +2,9 @@ defmodule GoogleDocsCloneWeb.PageController do
   use GoogleDocsCloneWeb, :controller
 
   def home(conn, _params) do
-    json(conn, %{message: "Welcome to the Google Docs Clone Backend!"})
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
   end
 
   def ping(conn, _params) do
