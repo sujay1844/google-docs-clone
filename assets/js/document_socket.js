75f69ac36bdaf5608fa6e36b1b58713b00f8cee7
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-12-13 23:35:17 +0530

    Using esbuild to get quill

--- assets/js/document_socket.js
+++ assets/js/document_socket.js
@@ -1,85 +1,40 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
+import { Socket } from "phoenix";
+import Quill from "quill";
 
-// Bring in Phoenix channels client library:
-import {Socket} from "phoenix"
+let socket = new Socket("/socket", { params: { token: window.userToken } });
+socket.connect();
 
-// And connect to the path in "lib/google_docs_clone_web/endpoint.ex". We pass the
-// token for authentication. Read below how it should be used.
-let socket = new Socket("/socket", {params: {token: window.userToken}})
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/google_docs_clone_web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/google_docs_clone_web/templates/layout/app.html.heex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/3" function
-// in "lib/google_docs_clone_web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket, _connect_info) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1_209_600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, connect to the socket:
-socket.connect()
-
-// Now that you are connected, you can join channels with a topic.
-// Let's assume you have a channel with a topic named `room` and the
-// subtopic is its id - in this case 42:
-const id = document.getElementById("document-id").innerText
-const channel = socket.channel("document:" + id, {})
+const id = document.getElementById("document-id").innerText;
+const channel = socket.channel("document:" + id, {});
 
 const quill = new Quill("#editor", {
   theme: "snow",
   modules: {
-    toolbar: [
-      ["bold", "italic", "underline"],
-    ]
-  }
-})
+    toolbar: [["bold", "italic", "underline"]],
+  },
+});
 
-quill.on("text-change", (delta, oldDelta, source) => {
+quill.on("text-change", (delta, _, source) => {
   if (source !== "user") return;
-  channel.push("delta", {body: delta})
-})
+  channel.push("delta", { body: delta, revision: 0 });
+});
 
-channel.on("delta", ({body}) => {
-  console.log("received delta", body)
-  quill.updateContents(body)
-})
+channel.on("delta", ({ body }) => {
+  console.log("received delta", body);
+  quill.updateContents(body);
+});
 
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+channel.on("ack", ({ revision }) => {
+  console.log("received ack", revision);
+});
 
-export default socket
+channel
+  .join()
+  .receive("ok", (resp) => {
+    console.log("Joined successfully", resp);
+  })
+  .receive("error", (resp) => {
+    console.log("Unable to join", resp);
+  });
+
+export default socket;
