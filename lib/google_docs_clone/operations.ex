30c09b3194e5a9f497a9d260a6237babaa251975
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-12-15 11:06:01 +0530

    Add operations object in database

---
+++ lib/google_docs_clone/operations.ex
@@ -0,0 +1,23 @@
+defmodule GoogleDocsClone.Operations do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "operations" do
+    belongs_to :document, GoogleDocsClone.Documents
+
+    field :user_id, :string
+    field :type, :string
+    field :position, :integer
+    field :content, :string
+    field :length, :integer
+
+    timestamps(type: :utc_datetime)
+  end
+
+  def changeset(operation, params \\ %{}) do
+    operation
+    |> cast(params, [:document_id, :user_id, :type, :position, :content, :length])
+    |> validate_required([:document_id, :type])
+    |> foreign_key_constraint(:document_id)
+  end
+end
---
+++ priv/repo/migrations/20241214184535_create_operations.exs
@@ -0,0 +1,18 @@
+defmodule GoogleDocsClone.Repo.Migrations.CreateOperations do
+  use Ecto.Migration
+
+  def change do
+    create table(:operations) do
+      add :document_id, references(:documents, on_delete: :delete_all)
+      add :user_id, :string
+      add :type, :string
+      add :position, :integer
+      add :content, :string
+      add :length, :integer
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:operations, [:document_id])
+  end
+end
